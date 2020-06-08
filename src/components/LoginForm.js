e41e07e28a1d3b061c500b99f391d55abe71d077
aseleidy <ash.etemadi@gmail.com>
2020-06-08 16:54:11 -0400

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,69 @@
 import React from "react";
 
 class LoginForm extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
+  }
+
+  // handleUsernameChange = (event) => {
+  //   this.setState({
+  //     username: event.target.value
+  //   })
+  // }
+
+  // handlePasswordChange = (event) => {
+  //   this.setState({
+  //     password: event.target.value
+  //   })
+  // }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  } 
+
+  handleSumbit = (event) => {
+    event.preventDefault();
+
+    if (this.state.username !== '' && this.state.password !== '') {
+      this.props.handleLogin(this.state)
+    } else {
+      console.log('Please enter a username and password')
+    }
+
+    this.setState({
+      username: '',
+      password: ''
+    })
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSumbit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" 
+            name="username" 
+            type="text" 
+            value={this.state.username}
+            onChange={this.handleChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" 
+            name="password" 
+            type="password" 
+            value={this.state.password}
+            onChange={this.handleChange} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,33 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
+  constructor(props) {
     super();
 
-    this.state = {};
+    this.state = {
+      maxChars: props.maxChars,
+      message: '',
+      charsLeft: props.maxChars
+    };
+  }
+
+  handleMessageInput = (event) => {
+    let charsCount = event.target.value.length
+    const charsLeft = this.state.maxChars - charsCount
+
+    this.setState({
+      message: event.target.value,
+      charsLeft: charsLeft
+    })
   }
 
   render() {
+    // console.log(this.state)
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input onChange={(event) => this.handleMessageInput(event)} type="text" name="message" id="message" value={this.state.message} />
+        <p>{this.state.charsLeft} Characters Left</p>
       </div>
     );
   }
